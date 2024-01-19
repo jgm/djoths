d78726637e11b1f00e87ace112cef9b73e3780d4
John MacFarlane <jgm@berkeley.edu>
2024-01-19 10:42:30 -0800

    consolidate: use a slightly faster foldr.

--- src/Djot/Inlines.hs
+++ src/Djot/Inlines.hs
@@ -82,16 +82,16 @@ pInlines = skipMany ws *> (consolidate . mconcat <$> many pInline)
 -- so that only the "x" gets following attributes.
 -- e.g.  *x{.foo}
 consolidate :: Inlines -> Inlines
-consolidate (Inlines ils') = Inlines (foldl' go mempty ils')
+consolidate (Inlines ils') = Inlines (foldr go mempty ils')
  where
-   go ils il@(Node attr (Str s)) =
-     case Seq.viewr ils of
-              start Seq.:> Node (Attr []) (Str s')
-                | not (B8.null s')
-                , not (isWs (B8.last s'))
-                -> start Seq.|> Node attr (Str (s' <> s))
-              _ -> ils Seq.|> il
-   go ils il = ils Seq.|> il
+   go il@(Node (Attr []) (Str s)) ils
+      | not (B8.null s)
+      , not (isWs (B8.last s))
+      = case Seq.viewl ils of
+              Node attr (Str s') Seq.:< end
+                -> Node attr (Str (s <> s')) Seq.<| end
+              _ -> il Seq.<| ils
+   go il ils = il Seq.<| ils
 
 pInline :: P Inlines
 pInline = pInline' >>= pOptionalAttributes
