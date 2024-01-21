888cbb6a1cb0e65d3e97d5cc4d5de7d446d61f65
John MacFarlane <jgm@berkeley.edu>
2024-01-21 13:35:48 -0800

    Add benchmark for djot writer.

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -5,10 +5,12 @@
 import Test.Tasty.Bench
 import Data.Functor.Identity  -- base >= 4.8
 import qualified Data.ByteString as B
-import Djot ( ParseOptions(..), parseDoc )
+import Djot ( ParseOptions(..), parseDoc, renderHtml, renderDjot )
 import Data.ByteString.Builder ( toLazyByteString )
+import Text.DocLayout (render)
 import System.Directory
 import System.FilePath (takeExtension, (</>))
+import qualified Data.ByteString.Lazy as BL
 
 main :: IO ()
 main = do
@@ -16,5 +18,15 @@ main = do
   files <- mapM (\fn -> (fn,) <$> B.readFile ("benchmark" </> fn)) fns
   defaultMain $
    map (\(fn, bs) ->
-     bench fn $ whnf (parseDoc ParseOptions{ optSourcePositions = False }) bs)
+     bench ("parse " <> fn) $ whnf (parseDoc ParseOptions{ optSourcePositions = False }) bs)
+     files
+   ++
+   map (\(fn, bs) ->
+     let doc = either error id $ parseDoc ParseOptions{ optSourcePositions = False } bs
+     in bench ("renderHtml " <> fn) $ whnf (BL.toStrict . toLazyByteString . renderHtml) doc)
+     files
+   ++
+   map (\(fn, bs) ->
+     let doc = either error id $ parseDoc ParseOptions{ optSourcePositions = False } bs
+     in bench ("renderDjot " <> fn) $ whnf (render (Just 72) . renderDjot) doc)
      files
