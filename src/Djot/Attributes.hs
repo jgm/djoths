0458a326ae01bfd25ba4e8c10228a2c3350a0537
John MacFarlane <jgm@berkeley.edu>
2024-01-21 13:22:10 -0800

    Removed redundant code in Attribute.

--- src/Djot/Attributes.hs
+++ src/Djot/Attributes.hs
@@ -126,40 +126,6 @@ pAttributes = try $ do
 
 --- old parser ---
 
-pAttributes' :: ParserT m s String Attr
-pAttributes' = try $ do
-  asciiChar' '{'
-  atts <- (do
-    skipMany pIgnorable
-    x <- pAttribute
-    xs <- many (try (skipSome pIgnorable *> pAttribute))
-    pure $ mconcat (x:xs)) <|> pure mempty
-  skipMany pIgnorable
-  asciiChar' '}'
-  pure atts
-
-pAttribute :: ParserT m s String Attr
-pAttribute = pIdentifier <|> pClass <|> pKeyVal
-
-pIdentifier :: ParserT m s String Attr
-pIdentifier = do
-  asciiChar' '#'
-  name <- pName
-  pure $ Attr [("id", name)]
-
-pClass :: ParserT m s String Attr
-pClass = do
-  asciiChar' '.'
-  name <- pName
-  pure $ Attr [("class", name)]
-
-pKeyVal :: ParserT m s String Attr
-pKeyVal = do
-  key <- pKey
-  asciiChar' '='
-  val <- pVal
-  pure $ Attr [(key, val)]
-
 pName :: ParserT m s String ByteString
 pName = byteStringOf $ skipSome $
   skipSatisfy (\c -> not (isSpace c)
@@ -171,27 +137,5 @@ pKey = byteStringOf $ skipSome $ skipSatisfyAscii isKeyChar
 isKeyChar :: Char -> Bool
 isKeyChar c = isAlphaNum c || c == ':' || c == '_' || c == '-'
 
-pVal :: ParserT m s String ByteString
-pVal = pQuotedVal <|> pBareVal
-
-pQuotedVal :: ParserT m s String ByteString
-pQuotedVal = do
-  asciiChar' '"'
-  result <- some $
-                (asciiChar' '\\' *> satisfy' (const True))
-            <|> (' ' <$ skipSome ws)
-            <|> satisfy' (/= '"')
-  asciiChar' '"'
-  pure $ strToUtf8 result
-
 pBareVal :: ParserT m s String ByteString
 pBareVal = pKey
-
-pComment :: ParserT m s String ()
-pComment = do
-  asciiChar' '%'
-  skipMany $ satisfy (\c -> c /= '}' && c /= '%')
-  asciiChar' '%' <|> lookahead (asciiChar' '}')
-
-pIgnorable :: ParserT m s String ()
-pIgnorable = ws <|> pComment
