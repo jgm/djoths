7901497eea9dff81f52edac58ca249f5ad7dc18c
John MacFarlane <jgm@berkeley.edu>
2024-01-24 10:06:22 -0800

    Add options for wrapping to CLI.

--- app/Main.hs
+++ app/Main.hs
@@ -2,33 +2,67 @@
 {-# LANGUAGE Strict #-}
 module Main where
 
-import Control.Monad
 import qualified Data.ByteString as B
 import Data.ByteString.Builder (hPutBuilder)
 import Djot ( ParseOptions(..), RenderOptions(..),
               parseDoc, renderHtml, renderDjot )
 import System.Environment (getArgs)
 import System.IO (stderr, stdout, hPutStrLn)
-import System.Exit ( ExitCode(ExitFailure, ExitSuccess), exitWith )
+import System.Exit ( ExitCode(ExitFailure), exitWith, exitSuccess )
 import Text.DocLayout (render)
+import Text.Read (readMaybe)
 import qualified Data.Text.IO as TIO
 
 data OutputFormat = Html | Djot | Ast
+  deriving (Eq, Show)
+
+data WrapOption = Auto | Preserve | NoWrap
+  deriving (Eq, Show)
 
 data Opts =
       Opts{ format :: OutputFormat
-          , files :: [FilePath] }
+          , files :: [FilePath]
+          , wrap :: WrapOption
+          , columns :: Int }
 
 parseOpts :: [String] -> IO Opts
-parseOpts = foldM go Opts{ format = Html, files = [] }
+parseOpts = go Opts{ format = Html, files = [], wrap = Preserve, columns = 72 }
  where
-   go opts "--djot" = pure $ opts{ format = Djot }
-   go opts "--ast" = pure $ opts{ format = Ast }
-   go opts "--html" = pure $ opts{ format = Html }
-   go _opts ('-':xs) = do
-     hPutStrLn stderr $ "Unknown option " <> ('-':xs)
-     exitWith $ ExitFailure 1
-   go opts f = pure $ opts{ files = files opts ++ [f] }
+   go opts [] = pure opts
+   go opts ("--wrap" : as) =
+     case as of
+       "auto" : as' -> go opts{ wrap = Auto } as'
+       "preserve" : as' -> go opts{ wrap = Preserve } as'
+       "none" : as' -> go opts{ wrap = NoWrap } as'
+       _ -> err "--wrap must be followed by auto, preserve, or none"
+   go opts ("--columns" : as) =
+     case as of
+       (a:as') | Just n <- readMaybe a
+         -> go opts{ columns = n } as'
+       _ -> err "--columns must be followed by a number"
+   go opts ("--to" : as) =
+     case as of
+       "djot" : as' -> go opts{ format = Djot } as'
+       "html" : as' -> go opts{ format = Html } as'
+       "ast" : as' -> go opts{ format = Ast } as'
+       _ -> err "--to must be followed by djot, html, or ast"
+   go _opts ("--help" : _) = do
+     putStrLn "djoths [options] [files]"
+     putStrLn "  --to djot|html|ast"
+     putStrLn "  --wrap auto|preserve|none"
+     putStrLn "  --columns NUMBER"
+     putStrLn "  --help"
+     exitSuccess
+   go opts (xs@('-':_) : as) =
+     case break (== '=') xs of  -- support e.g. '--columns=33'
+       (ys, '=':zs) -> go opts (ys : zs : as)
+       _ -> err $ "Unknown option " <> ('-':xs)
+   go opts (f : as) = go opts{ files = files opts ++ [f] } as
+
+err :: String -> IO a
+err msg = do
+  hPutStrLn stderr msg
+  exitWith $ ExitFailure 1
 
 main :: IO ()
 main = do
@@ -37,14 +71,18 @@ main = do
           [] -> B.getContents
           fs  -> mconcat <$> mapM B.readFile fs
   let popts = ParseOptions
-  let ropts = RenderOptions { preserveSoftBreaks = True }
+  let ropts = RenderOptions { preserveSoftBreaks = wrap opts == Preserve }
   case parseDoc popts bs of
     Right doc -> do
       case format opts of
         Html -> hPutBuilder stdout $ renderHtml ropts doc
-        Djot -> TIO.putStr $ render Nothing (renderDjot ropts doc)
+        Djot -> TIO.putStr $ render (case wrap opts of
+                                       NoWrap -> Nothing
+                                       Preserve -> Nothing
+                                       Auto -> Just (columns opts))
+                           $ renderDjot ropts doc
         Ast -> print doc
-      exitWith ExitSuccess
+      exitSuccess
     Left e -> do
       hPutStrLn stderr e
       exitWith $ ExitFailure 1
