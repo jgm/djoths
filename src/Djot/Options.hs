0f5b02f1b4001165362341d613664cc84d597761
John MacFarlane <jgm@berkeley.edu>
2024-02-10 16:29:47 -0800

    Clarifying comment.

--- src/Djot/Options.hs
+++ src/Djot/Options.hs
@@ -5,18 +5,21 @@ module Djot.Options
 , SourcePosOption(..) )
 where
 
-data ParseOptions =
+newtype ParseOptions =
   ParseOptions
   { sourcePositions :: SourcePosOption -- ^ Add attributes for source lines
   }
   deriving (Show)
 
-data RenderOptions =
+newtype RenderOptions =
   RenderOptions
   { preserveSoftBreaks :: Bool -- ^ Preserve soft breaks as in the source
   }
   deriving (Show)
 
+-- | Adding source positions for blocks adds almost no overhead to parsing.
+-- Adding source positions for inlines has a small penalty.  For many purposes
+-- it is enough to have source lines for blocks, so we offer the option.
 data SourcePosOption =
   NoSourcePos | BlockSourcePos | AllSourcePos
   deriving (Show, Eq, Ord)
